8af704155e86a7f475d833af3b6e6086280dfd27
eduardoespinoza725 <45131602+eduardoespinoza725@users.noreply.github.com>
2022-08-31 21:02:54 -0500

    Update AstronautDB_DMLScript.sql

--- Sql_Samples/AstronautDB_DMLScript.sql
+++ Sql_Samples/AstronautDB_DMLScript.sql
@@ -8,61 +8,61 @@
 INSERT INTO eespinoz_Space_Agency
 (name, country, director, leadScientist, dateFounded)
 VALUES
-('NASA',               'United States',      'Bill Nelson',              'Pam Melroy',             '1958-07-29'),
-('CNSA',               'China',                  'Zhang Kejian',          'Wu Yanhua',             '1993-04-22'),
-('JAXA',                'Japan',                  'Yamakawa Hiroshi', 'Suzuki Kazuhiro',     '2003-10-01'),
-('ISRO',                'India',                    'Shri S. Somanath',   'Dr. P K Mishra',        '1968-08-15'),
-('ESA',                  'European Union', 'Josef Aschbacher',    'Torben Henriksen',  '1975-05-30'),
-('ROSCOSMOS', 'Russia',                 'Dmitry Rogozin',       'Vladimir Popovkin',  '1992-02-25'),
-('SpaceX',             'United States',      'Elon Musk',              'Tom Mueller',            '2002-03-14'),
-('Blue Origin',        'United States',      'Jeff Bezos',              'Christopher Jaeger', '2000-09-08'),
-('Virgin Galactic',   'United States',      'Richard Branson',    'Burt Rutan',              '2004-09-14'),
-('ASA',                   'Australia',             'Melissa Price',          'Enrico Palermo',       '2018-07-1')
+('NASA',               'United States',      'Bill Nelson',      'Pam Melroy',         '1958-07-29'),
+('CNSA',               'China',              'Zhang Kejian',     'Wu Yanhua',          '1993-04-22'),
+('JAXA',               'Japan',              'Yamakawa Hiroshi', 'Suzuki Kazuhiro',    '2003-10-01'),
+('ISRO',               'India',              'Shri S. Somanath', 'Dr. P K Mishra',     '1968-08-15'),
+('ESA',                'European Union',     'Josef Aschbacher', 'Torben Henriksen',   '1975-05-30'),
+('ROSCOSMOS',          'Russia',             'Dmitry Rogozin',    'Vladimir Popovkin', '1992-02-25'),
+('SpaceX',             'United States',      'Elon Musk',         'Tom Mueller',       '2002-03-14'),
+('Blue Origin',        'United States',      'Jeff Bezos',        'Christopher Jaeger','2000-09-08'),
+('Virgin Galactic',    'United States',      'Richard Branson',   'Burt Rutan',        '2004-09-14'),
+('ASA',                'Australia',          'Melissa Price',     'Enrico Palermo',    '2018-07-1')
 ;
 
 INSERT INTO eespinoz_Space_Mission
 (description, teamLeader, agencyID)
 VALUES
-('Fly to the moon', 									  'Kent Shannen',       1),
-('Observe sun',                                          'Zhen Shu',              2),
-('Observe distant planet',							  'Izumi Yuko',            3),
-('Collect data on distant stars', 				  'Samir Mohan',        4),
-('Test rocket fuel efficency', 						  'Vlado Hans-Peter', 5),
-('Successfully land unmaned rocket',        'Elliot Bryon',           6),
-('Send first private citizens to space',        'Veronica Aynura',   7),
-('Gather data on climate change',             'Shuhrat Candelas', 8),
-('Send a rover to Mars',                             'Alessia Tjeerd', 	  9),
-('Repair satellite in orbit',                           'Ranulph Dimos', 	 10)
+('Fly to the moon', 									   'Kent Shannen',     1),
+('Observe sun',                          'Zhen Shu',         2),
+('Observe distant planet',							 'Izumi Yuko',       3),
+('Collect data on distant stars', 			 'Samir Mohan',      4),
+('Test rocket fuel efficency', 					 'Vlado Hans-Peter', 5),
+('Successfully land unmaned rocket',     'Elliot Bryon',     6),
+('Send first private citizens to space', 'Veronica Aynura',  7),
+('Gather data on climate change',        'Shuhrat Candelas', 8),
+('Send a rover to Mars',                 'Alessia Tjeerd', 	 9),
+('Repair satellite in orbit',            'Ranulph Dimos', 	 10)
 ;
 
 INSERT INTO eespinoz_Astronaut
 (fName, lName, rank, age, dateEnlisted, completedMissions, agencyID)
 VALUES
-('Alessia',   'Tjeerd',  	   'Command Pilot',               30, '2018-05-25',  21,    9),
-('Ranulph', 'Dimos', 		   'Commander',                    45, '2019-10-12', 10,   10),
-('Zhen',      'Shu',             'Misson Specialist',           34, '2017-11-11',  13,     2),
-('Kent',       'Shannen',     'Spacecraft Commander', 29, '2021-02-29',  5,      1),
-('Elliot',      'Bryon',           'Pilot',                                53, '2010-04-30',  102,  5),
-('Samir',     'Mohan',         'Commander',                   44, '2020-12-23',  15,    4),
-('Shuhrat',  'Candelas',     'Spacecraft Commander', 34, '2021-07-03',  8,      8),
-('Veronica', 'Aynura',        'Spacecraft Pilot',              30, '2021-11-23',  10,    7),
-('Vlado',      'Hans-Peter', 'Pilot Cosmonaut',             55, '2002-09-26',  325,  6),
-('Izumi',      'Yuko',            'Commander',                   32, '2022-06-10',   0,     3)
+('Alessia',  'Tjeerd',  	   'Command Pilot',        30, '2018-05-25',  21,    9),
+('Ranulph',  'Dimos', 		   'Commander',            45, '2019-10-12',  10,    10),
+('Zhen',     'Shu',          'Misson Specialist',    34, '2017-11-11',  13,    2),
+('Kent',     'Shannen',      'Spacecraft Commander', 29, '2021-02-29',  5,     1),
+('Elliot',   'Bryon',        'Pilot',                53, '2010-04-30',  102,   5),
+('Samir',    'Mohan',        'Commander',            44, '2020-12-23',  15,    4),
+('Shuhrat',  'Candelas',     'Spacecraft Commander', 34, '2021-07-03',  8,     8),
+('Veronica', 'Aynura',       'Spacecraft Pilot',     30, '2021-11-23',  10,    7),
+('Vlado',    'Hans-Peter',   'Pilot Cosmonaut',      55, '2002-09-26',  325,   6),
+('Izumi',    'Yuko',         'Commander',            32, '2022-06-10',  0,     3)
 ;
 
 INSERT INTO eespinoz_Rocket_Ship
 (rocketClass, payload_kg, height_m, astroID)
 VALUES
-('Flyby',            24060, 120.52,   1),
-('Orbiter',          20532, 73.28,     2),
-('Orbiter',          21221, 101.45,   3),
+('Flyby',       24060, 120.52,   1),
+('Orbiter',     20532, 73.28,    2),
+('Orbiter',     21221, 101.45,   3),
 ('Atmospheric', 21330, 110.47,   4),
-('Lander',          23002, 115.34,   5),
-('Rover',            22300, 67.12,     6),
-('Observatory',  26700, 70.31,     7),
-('Orbiter',           21332, 87.87,    8),
-('Lander',           22100, 100.79,  9),
-('Orbiter',           27911,  90.44,  10)
+('Lander',      23002, 115.34,   5),
+('Rover',       22300, 67.12,    6),
+('Observatory', 26700, 70.31,    7),
+('Orbiter',     21332, 87.87,    8),
+('Lander',      22100, 100.79,   9),
+('Orbiter',     27911, 90.44,   10)
 ;
 
 INSERT INTO eespinoz_Mission_Objectives
@@ -70,7 +70,7 @@ INSERT INTO eespinoz_Mission_Objectives
 VALUES
 (1,  4,   'Gather lunar rocks'),
 (2,  3,   'Observe sun for unusual solar activity'),
-(3,  10, 'Search for more Earth-like planets'),
+(3,  10,  'Search for more Earth-like planets'),
 (4,  6,   'Observe star orbits'),
 (5,  9,   'Test minimum fuel needed'),
 (6,  5,   'Test landing sensors'),
@@ -79,6 +79,10 @@ VALUES
 (9,  1,   'Train astronauts for harsh conditons'),
 (10, 2,   'Prepare for spacewalk to do repairs')
 ;
+
+-- ********************************************************************************************************************************************************
+-- BASIC SQL OPERATIONS 
+-- ********************************************************************************************************************************************************
 
 -- ********************************************************************************
 --VALIDATE INSERTION OF AT LEAST 10 RECORDS IN ALL TABLES
